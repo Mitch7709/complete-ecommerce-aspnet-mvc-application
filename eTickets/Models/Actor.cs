1a1d3c27580d2ae9c85f377070df8c0e08476f98
Mitchell Lapora <mlapora1@gmail.com>
2023-10-09 13:38:09 -0500

    Added service actions for updating and deleting data for actors.

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -8,13 +8,18 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
         
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must between 3 and 50 characters")]
         public string FullName { get; set;}
+
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set;}
 
         //Relationships
-        public List<Actor_Movie> Actors_Movies{ get; set; }
+        public List<Actor_Movie>? Actors_Movies{ get; set; }
     }
 }
